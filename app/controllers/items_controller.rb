0e8b6ab5c24b2c9caaef01b7faca09bee6afc437
Roman Polidario Jr <rdpolidario@yahoo.com>
2015-04-13 12:17:22 +0000

    product category update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    if params[:category] == ''
+    if params[:category].blank?
       @items = Item.all
     else
       @items = Item.where(category: params[:category])
